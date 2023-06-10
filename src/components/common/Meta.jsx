0d1c04a4dfe041f0ec1207f5317aa3de2f536ac9
edidiesky <essienedidiong1000@gmail.com>
2023-06-10 01:07:45 -0700

    new fixes

--- src/components/common/Meta.jsx
+++ src/components/common/Meta.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import {Helmet} from "react-helmet";
-import Styled from 'styled-components'
+import React from "react";
+import { Helmet } from "react-helmet";
+import Styled from "styled-components";
 const MetaContainer = Styled.div`
 width:100%;
 .MetaWrapper {
 
 }
-` 
-export default function Meta({title, keyword, description}) {
-
-
-	return (
-		<Helmet>
-            <title>{title}</title>
-            <meta name="keyword" keyword={keyword} />
-            <meta name="description" description={description} />
-        </Helmet>
-		)
+`;
+export default function Meta({ title, keyword, description }) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="keyword" keyword={keyword} />
+      <meta name="description" description={description} />
+    </Helmet>
+  );
 }
-
 
 Meta.defaultProps = {
-	title:'Welcome to Ava shop- Home of Afforadble and durable car products',
-	description:'We sell qualtiy Cars',
-	keyword:'Cars, buy quality Cars, rare ones'
-}
-
+  description:
+    "Xverse is the most advanced Bitcoin Wallet. Earn Btn, trade NFTs and connect to Web3 apps powered by Stacks. Orginals fully supported. Download for iOS and Android and Chrome",
+  keyword: "Bitcoin walet for Web3",
+};
